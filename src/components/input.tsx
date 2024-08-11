f7545da281fd0b0727b822ced0dd080da9182328
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-08-10 22:34:47 -0300

    feat: create profile interface

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -3,23 +3,25 @@ import { ComponentProps } from "react";
 
 type InputParams = ComponentProps<typeof InputField>;
 
-export function Input({ ...rest }: InputParams) {
+export function Input({ editable = true, ...rest }: InputParams) {
   return (
     <GlueStackInput
-      bg="$gray700"
       h="$14"
-      px="$4"
       borderWidth="$0"
       borderRadius="$md"
       $focus={{
         borderWidth: 1,
         borderColor: "$green500",
       }}
+      opacity={editable ? 1 : 0.5}
     >
       <InputField
+        px="$4"
+        bg="$gray700"
         color="$white"
         fontFamily="$body"
         placeholderTextColor="$gray300"
+        editable={editable}
         {...rest}
       />
     </GlueStackInput>
--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,9 +1,67 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { Button } from "@components/button";
+import { Input } from "@components/input";
+import { ProfilePhoto } from "@components/profile-photo";
+import { ScreenHeader } from "@components/screen-header";
+import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
+import { ScrollView, TouchableOpacity } from "react-native";
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text color="$white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <ProfilePhoto
+            source={{ uri: "https://github.com/moesiomarcelino.png" }}
+            alt="User image"
+            size="xl"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input
+              value="moesiomarcelino1@gmail.com"
+              editable={false}
+              bg="$gray600"
+            />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+
+            <Button title="Atualizar" />
+          </Center>
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
